04a1c5b79e5c8e367381f050ed13dc593b5dca73
LawlessHeaven <lielais1997@inbox.lv>
2018-10-05 21:49:48 +0300

    Object from angular to java

--- src/main/webapp/app/entities/tests/studentTests-view.component.ts
+++ src/main/webapp/app/entities/tests/studentTests-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ITests } from 'app/shared/model/tests.model';
+import {TestRequestModel} from "app/shared/model/testRequest.model";
 
 @Component({
     selector: 'jhi-studentTests-view',
@@ -11,6 +12,7 @@ export class TestsViewDetailComponent implements OnInit {
     tests: ITests;
     options: any = [];
     option: any = [];
+
 
     constructor(private activatedRoute: ActivatedRoute) {}
 
@@ -23,4 +25,18 @@ export class TestsViewDetailComponent implements OnInit {
     previousState() {
         window.history.back();
     }
+
+
+
+    onSubmit() {
+        var request  = new TestRequestModel(this.tests.id , this.options);
+        console.log("TESTING SUBMIT BUTTON");
+        console.log(this.options);
+        console.log(this.option);
+
+    }
+
+
+
+
 }
